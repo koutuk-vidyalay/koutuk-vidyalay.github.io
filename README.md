21cdb187c39599369a2278cb29496b69c42b6fa0
Omkar Kulkarni <kulkarni.ok.01@gmail.com>
2022-07-11 11:12:58 +0530

    श्री

--- README.md
+++
@@ -1,1 +0,0 @@
-# koutuk-vidyalay.github.io
---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.querySelector('#info').onclick = function(){alert(
+"How to download BOOKS..!\n\n 1. Go to new page. \n 2. Click downlad. \n 3. Click save file. \n 4. At last extract zip file."
+
+)}
+
+
+const details = document.querySelectorAll(".bookDetails");
+details.forEach((targetDetail) => { targetDetail.addEventListener("click", () => { 
+ details.forEach((detail) => { if (detail !== targetDetail) { detail.removeAttribute("open"); } }); }); });
