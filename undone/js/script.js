6358d1346c48f2b45ec5d829c4d84fc6f3aca104
आचार्य Technologies <kulkarni.ok.01@gmail.com>
2022-08-24 08:54:29 +0530

    श्री

--- undone/js/script.js
+++
@@ -1,2 +0,0 @@
-document.querySelector('#info').onclick = function(){confirm("How to download BOOKS..!\n\n 1. Go to new page. \n 2. Click downlad. \n 3. Click save file. \n 4. At last extract zip file."
-)}
